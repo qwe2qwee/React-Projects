eb2a5cb1b80aa9fdde4f1c98b4b7d8afc084b43c
Hossin <qwe2qwe@hotmail.com>
2023-07-19 23:39:59 +0300

    add about and services sections

--- mustaff/src/components/About/About.jsx
+++ mustaff/src/components/About/About.jsx
@@ -1,13 +1,63 @@
 import React from "react";
-import "./About.css"
+import "./About.css";
+import about from "../../assets/Imgs/about.jpg";
+import AboutBox from "./AboutBox";
 
-
-const About = ()=> {
+const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className='about countainer section'>
+      <h2 className='section__title'>About Me</h2>
+      <div className='about__countainer grid'>
+        <div className="left">
+           <img src={about} alt='' className='about__img' />
+        </div>
+       
+        <div className='about__data grid'>
+          <div className='about__info'>
+            <p className='about__description'>
+              I am Mustafa Hassan, Graphic Designer from Jeddah,Saudi Arabia. I have
+              rich experience in design and Photograph and video editing,
+              also I am good at Motion Graphic.
+            </p>
+            <a href='' className='btn'>
+              {" "}
+              Download CV
+            </a>
+          </div>
+          <div className='about__skills grid'>
+            <div className='skills__data'>
+              <div className='skills__titles'>
+                <h3 className='skills__name '>Graphic Design</h3>
+                <span className='skills__number'>92%</span>
+              </div>
+              <div className='skills__bar'>
+                <span className='skills__percentage graphic-Design'></span>
+              </div>
+            </div>
+            <div className='skills__data'>
+              <div className='skills__titles'>
+                <h3 className='skills__name '>Photography</h3>
+                <span className='skills__number'>80%</span>
+              </div>
+              <div className='skills__bar'>
+                <span className='skills__percentage photography'></span>
+              </div>
+            </div>
+            <div className='skills__data'>
+              <div className='skills__titles'>
+                <h3 className='skills__name '>Video editing</h3>
+                <span className='skills__number'>75%</span>
+              </div>
+              <div className='skills__bar'>
+                <span className='skills__percentage video-editing'></span>
+              </div>
+            </div>
+          </div>   
+        </div>
+      </div>
+      <AboutBox/>
+    </section>
+  );
+};
 
-
-
-export default About
+export default About;
